86b63a1e1398d9c44af8224def7376cf03f4221a
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-04-25 21:48:36 -0300

    Conect firebase firestore

--- app/src/main/java/com/example/vendaskaefistore/LoginActivity.kt
+++ app/src/main/java/com/example/vendaskaefistore/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.example.vendaskaefistore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vendaskaefistore.databinding.ActivityLoginBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+       val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val db = Firebase.firestore
     }
 }
